4abb005c751d00c83571359d8050b26fc7c6e7f9
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2017-09-18 10:16:32 +0200

    Generated gh-pages for commit e9244d6 Author: Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
        Trial and erroring my way around here...

---
+++ make_gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+
+git checkout gh-pages
+rm -rf _images _modules _sources _static _stubs
+git checkout master docs *.py *.pyx
+git reset HEAD
+cd docs/
+make html
+cd ..
+mv -fv docs/build/html/* ./
+rm -rf docs *.py *.pyc *.pyx
+git add -A
+git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
